c17214ee625f2255ca582a8069e11844d449c2d4
Chris Barclay <chris.barclay@barefootsolutions.com>
2019-12-02 17:52:46 -0800

    Trying to push to remote repo -2nd try

--- src/index.js
+++ src/index.js
@@ -1,8 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import faker from "faker";
+import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
-    return <div>Hi there!</div>;
+  return (
+    <div className="ui container comments">
+      <ApprovalCard>
+        <div>
+          <h4>Warning!</h4>
+          Are you sure you want to do this?
+        </div>
+      </ApprovalCard>
+      
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 12:53PM"
+          content="Helloooo there."
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          timeAgo="Today at 11:14AM"
+          content="Oooweee look at me!"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          timeAgo="Yesterday at 6:17PM"
+          content="First comment!"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+    </div>
+  );
 };
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector("#root"));
